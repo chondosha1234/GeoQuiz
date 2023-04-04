4ccf97164088824519791d56d42b38e0e3c377a9
chondosha1234 <jmiller61193@gmail.com>
2023-04-04 18:03:06 -0400

    added cheatActivity and layout and connected to mainactivity

---
+++ app/src/main/java/com/chondosha/geoquiz/CheatActivity.kt
@@ -0,0 +1,40 @@
+package com.chondosha.geoquiz
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chondosha.geoquiz.databinding.ActivityCheatBinding
+
+private const val EXTRA_ANSWER_IS_TRUE = "com.chondosha.geoquiz.answer_is_true"
+
+private var answerIsTrue = false
+
+class CheatActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCheatBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCheatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+
+        binding.showAnswerButton.setOnClickListener {
+            val answerText = when {
+                answerIsTrue -> R.string.true_button
+                else -> R.string.false_button
+            }
+            binding.answerTextView.setText(answerText)
+        }
+    }
+
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }
+    }
+}
--- app/src/main/java/com/chondosha/geoquiz/MainActivity.kt
+++ app/src/main/java/com/chondosha/geoquiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.chondosha.geoquiz
 
+import android.content.Intent
 import android.icu.lang.UCharacter.GraphemeClusterBreak.L
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import com.chondosha.geoquiz.databinding.ActivityMainBinding
 
@@ -18,6 +20,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private val quizViewModel: QuizViewModel by viewModels() // property delegate -- creates the viewmodel when needed, don't directly instantiate
+
+    private val cheatLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +45,12 @@ class MainActivity : AppCompatActivity() {
         binding.nextButton.setOnClickListener {
             quizViewModel.moveToNext()
             updateQuestion()
+        }
+
+        binding.cheatButton.setOnClickListener {
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            startActivity(intent)
         }
 
         updateQuestion()
